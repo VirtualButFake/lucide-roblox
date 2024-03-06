c313917c22098b77a3417bca2ceb6c493bd96ad9
Virtual <officialimvirtual@gmail.com>
2024-03-06 23:00:49 +0100

    fix: fix issue in init.lua and fix lint issues

--- src/init.lua
+++ src/init.lua
@@ -1,38 +1,38 @@
 local lucideRoblox = {}
 
 local icons = require("icons")
-local iconIndex: { string } = icons[1]
-local idIndex: { number } = icons[2]
+local iconIndices: { string } = icons[1]
+local idIndices: { number } = icons[2]
 local iconRegistry: { [number]: { number | { number } } } = icons[3]
 
-lucideRoblox.icons = iconIndex
+lucideRoblox.icons = iconIndices
 
 function lucideRoblox.GetAsset(name: string, size: number?): icon?
 	if not size then
 		size = 48
 	end
 
-	local iconIndex = table.find(iconIndex, name)
+	local iconIndex = table.find(iconIndices, name)
 
 	if not iconIndex then
 		return
 	end
 
 	local currentDistance = math.huge
-	for registrySize, icons in iconRegistry do
+	for registrySize, iconList in iconRegistry do
 		local diff = math.abs(size - registrySize)
 
 		if diff < currentDistance then
 			currentDistance = diff
 		else
-			local icon = icons[iconIndex]
+			local icon = iconList[iconIndex]
 
 			return {
 				name = name,
-				id = idIndex[icon[1]],
-				url = "rbxassetid://" .. idIndex[icon[1]],
+				id = idIndices[icon[1]],
+				url = "rbxassetid://" .. idIndices[icon[1]],
 				imageRectSize = Vector2.new(icon[2][1], icon[2][1]),
-				imageRectOffset = Vector2.new(icon[3][1], icons[3][1]),
+				imageRectOffset = Vector2.new(icon[3][1], icon[3][2]),
 			}
 		end
 	end
